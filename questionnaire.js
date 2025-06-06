d859b3705de7d6f5c054a708383d8cc9d49fc938
jodiew9 <jodie.williams@learnlux.com>
2025-06-06 15:02:23 -0700

    Create questionnaire.js

---
+++ questionnaire.js
@@ -0,0 +1,8 @@
+const { useState } = React;
+const { ChevronRight, TrendingUp, Shield, AlertTriangle, DollarSign } = lucide;
+
+const RiskToleranceQuestionnaire = () => {
+  // [Copy the entire component code from the original artifact here]
+};
+
+ReactDOM.render(React.createElement(RiskToleranceQuestionnaire), document.getElementById('root'));
